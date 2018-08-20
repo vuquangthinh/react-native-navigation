cb4900943c32bf904b4253fcb9784b451ae77284
Quang Thinh <quangthinh.dico@gmail.com>
2018-08-20 14:41:43 +0700

    fix ios11

---
+++ ios/RCCTabBarController.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+#import <React/RCTBridge.h>
+
+@interface RCCTabBarController : UITabBarController <UITabBarDelegate>
+
+- (instancetype)initWithProps:(NSDictionary *)props children:(NSArray *)children globalProps:(NSDictionary*)globalProps bridge:(RCTBridge *)bridge;
+- (void)performAction:(NSString*)performAction actionParams:(NSDictionary*)actionParams bridge:(RCTBridge *)bridge completion:(void (^)(void))completion;
+
+- (UIImage*) changeViewToImage : (UIView*) viewForImage;
+
+
+
+@property (nonatomic) BOOL tabBarHidden;
+
+@property NSArray* tabsProp;
+
+@end
